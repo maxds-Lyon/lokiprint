9d3e38e1af478a58b05451ddf40020e46f69f9e1
Alma Rozanès <alma.rozanes@gmail.com>
2024-03-14 14:04:23 +0100

    Use absolute schema path

--- createValidate.mjs
+++ createValidate.mjs
@@ -1,9 +1,10 @@
 import { promises as fs } from 'fs';
 import Ajv from "ajv";
 import addFormats from "ajv-formats";
+import { join } from 'path';
 
 export const createValidate = async () => {
-    const schema = JSON.parse(await fs.readFile('./people.schema.json', 'utf-8'));
+    const schema = JSON.parse(await fs.readFile(join(import.meta.dirname, 'people.schema.json'), 'utf-8'));
     const ajv = new Ajv({ allErrors: true });
     addFormats(ajv);
     return ajv.compile(schema);
